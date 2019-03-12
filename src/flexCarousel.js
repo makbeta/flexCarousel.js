3b9b08e4caaadf9f0d3d17f9a9cc4663bff5ce70
Tom Hartley <tom@tomhrtly.com>
2019-03-12 21:44:32 +0000

    Add center mode functionality

--- src/flexCarousel.js
+++ src/flexCarousel.js
@@ -32,11 +32,12 @@
         arrowsOverlay: true,
         autoplay: false,
         autoplaySpeed: 5000,
+        center: false,
         height: null,
         nextArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>',
         prevArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
         slidesVisible: 1,
-        transition: 'slide',
+        transition: 'slide'
       };
 
       // Combine both objects
@@ -115,7 +116,12 @@
     var slides = self.selector.find('.fc-slides');
 
     if(self.options.slidesVisible < slide.length) {
-      slides.css('left', '-' + slideWidth);
+      if(self.options.center) {
+        slides.css('left', 'calc(-70% / ' + self.options.slidesVisible + ')');
+      } else {
+        slides.css('left', 'calc(100% / ' + self.options.slidesVisible + ')');
+      }
+
       slide.last().css('order', 1);
 
       var i = 2;
@@ -136,11 +142,15 @@
       });
 
       if(self.options.transition === 'slide') {
-        slides.css('transform', 'translateX(' + slideWidth + ')');
+        slides.css('transform', 'translateX(80%)');
       }
     }
 
-    slide.css('min-width', 'calc(100% / ' + self.options.slidesVisible + ')');
+    if(self.options.center) {
+      slide.css('min-width', 'calc(80% / ' + self.options.slidesVisible + ')');
+    } else {
+      slide.css('min-width', 'calc(100% / ' + self.options.slidesVisible + ')');
+    }
   }
 
   object.changeOrder = function(amount) {
@@ -163,7 +173,7 @@
 
       // Determine whether the carousel is going forward or backward
       if(self.options.transition === 'slide') {
-        slides.css('transform', 'translateX(-' + slideWidth + ')');
+        slides.css('transform', 'translateX(-80%)');
       }
     } else {
       slide.each(function() {
@@ -179,7 +189,7 @@
 
       // Determine whether the carousel is going forward or backward
       if(self.options.transition === 'slide') {
-        slides.css('transform', 'translateX(' + slideWidth + ')');
+        slides.css('transform', 'translateX(80%)');
       }
     }
   }
